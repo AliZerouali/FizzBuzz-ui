3e5fa790950332adaf19cd5b023687cd88344019
ali zerouali <ali.zerouali@dxc.com>
2026-01-29 21:25:58 +0100

    fix URL

--- fizzbuzz-ui/src/services/api.ts
+++ fizzbuzz-ui/src/services/api.ts
@@ -5,7 +5,7 @@ import {
   StatisticsResponse,
 } from "../types";
 
-const API_BASE_URL = "https://localhost:5000/api";
+const API_BASE_URL = "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
